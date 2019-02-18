6b7cb131362ec433ba2ce9c0a923d55dcd4459e5
begueradj <begueradj.bill@gmail.com>
2019-02-18 15:25:56 +0200

    Added append function to singly linked list data structure

---
+++ linked-lists/singly-linked-lists/singlyLinkedList.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function singlyLinkedList() {
+  this.head = null
+}
+
+
+singlyLinkedList.prototype.append = function(value) {
+  let node = {
+    data: value,
+    next: null
+  }
+  if( !this.head ) {
+    this.head = node
+  } else {
+    let pointer = this.head
+    while( pointer.next ) {
+      pointer = pointer.next
+    }
+    pointer.next = node
+  }
+}
+
