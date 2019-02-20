bdac0886513765843cdd8604315d157a4e3b4382
begueradj <billal.begueradj@fisplay.com>
2019-02-20 17:08:39 +0200

    Added remove value and read functions

--- linked-lists/singly-linked-lists/singlyLinkedList.js
+++ linked-lists/singly-linked-lists/singlyLinkedList.js
@@ -4,7 +4,7 @@ let singlyLinkedList = function () {
   this.head = null
 }
 
-singlyLinkedList.prototype.append = function(value) {
+singlyLinkedList.prototype.append = function (value) {
   let node = {
     data: value,
     next: null
@@ -20,8 +20,42 @@ singlyLinkedList.prototype.append = function(value) {
   }
 }
 
+singlyLinkedList.prototype.remove = function (value) {
+  if(! this.head) {
+    console.log('The list is empty')
+  } else if ( this.head.data === value ) {
+    this.head = this.head.next
+  } else {
+    let first = this.head
+    let second = first.next
+    while( second ) {
+      if ( second.data === value ) {
+	first.next = second.next
+	break
+      } else {
+	first = second
+      }
+      second = second.next
+    }   
+  }  
+}
+
+singlyLinkedList.prototype.read = function () {
+  if ( !this.head ) {
+    console.log('Empty list')
+  } else {
+    let current = this.head
+    while ( current ) {
+      console.log ( current.data )
+      current = current.next
+    }
+  }
+}
+
 let linkedList = new singlyLinkedList()
 let someIntegers = [3, 55, 632, 7, 9]
 someIntegers.forEach(element => linkedList.append( element ) )
-console.log(linkedList)
-
+console.log( linkedList.read() )
+console.log('remove 7 ')
+linkedList.remove(7)
+console.log(linkedList.read() )
